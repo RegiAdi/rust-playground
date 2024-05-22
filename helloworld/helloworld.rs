fcffe28964fcdd5482a9e9f9ae2e642a6001a774
Regi Adi <regiadi.priyanto@gmail.com>
2024-05-23 01:07:49 +0700

    Add helloworld

---
+++ helloworld/helloworld.rs
@@ -0,0 +1,29 @@
+// This is a comment
+// This is a comment
+// This is a comment
+
+// This is the main function
+fn main() {
+    println!("1 Hello World!");
+    println!("2 Hello World!");
+    println!("3 Hello World!");
+    println!("4 Hello World!");
+    println!("5 Hello World!");
+    println!("6 Hello World!");
+    println!("7 Hello World!");
+    println!("8 Hello World!");
+    println!("9 Hello World!");
+    println!("10 Hello World!");
+    println!("11 Hello World!");
+    println!("12 Hello World!");
+    println!("13 Hello World!");
+    println!("14 Hello World!");
+    println!("15 Hello World!");
+    println!("16 Hello World!");
+    println!("17 Hello World!");
+    println!("18 Hello World!");
+    println!("19 Hello World!");
+    println!("20 Hello World!");
+    println!("21 Hello World!");
+    println!("22 Hello World!");
+}
