418a4e3ccf3c2846fea2a5f3144958cedaf9d41b
Regi Adi <regiadi.priyanto@gmail.com>
2024-06-06 20:13:40 +0700

    Add functions

---
+++ functions/functions.rs
@@ -0,0 +1,19 @@
+fn main() {
+    println!("Hello, World!");
+
+    another_function();
+    more_function();
+    some_function();
+}
+
+fn another_function() {
+    println!("Another function.");
+}
+
+fn more_function() {
+    println!("More function.");
+}
+
+fn some_function() {
+    println!("Some function.");
+}
