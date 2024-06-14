1884c9c212fedbf60224b7047c71ed75936ae197
Regi Adi <regiadi.priyanto@gmail.com>
2024-06-14 13:44:29 +0700

    Add control_flow/for

---
+++ control_flow/for.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let a = [100, 200, 300, 400, 500];
+
+    for element in a {
+        println!("{element}");
+    }
+
+    for number in 1..4 {
+        println!("{number}");
+    }
+}
